7dce191c9fe82c604b334c0454bee2ddd78c317c
juangomezru <128845389+juangomezru@users.noreply.github.com>
2024-05-01 13:15:46 -0500

    Initial commit

---
+++ src/main/java/com/unisabana/airport/AirportDddApplication.java
@@ -0,0 +1,13 @@
+package com.unisabana.airport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportDddApplication.class, args);
+	}
+
+}
