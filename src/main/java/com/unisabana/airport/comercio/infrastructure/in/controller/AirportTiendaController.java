acc316b0dd8793bd4efcd00f255f83ee6af0782e
jeanvaes <jeanvaes@unisabana.edu.co>
2024-05-03 20:31:21 -0500

    Arreglar endpoint

--- src/main/java/com/unisabana/airport/comercio/infrastructure/in/controller/AirportTiendaController.java
+++ src/main/java/com/unisabana/airport/comercio/infrastructure/in/controller/AirportTiendaController.java
@@ -1,9 +1,7 @@
 package com.unisabana.airport.comercio.infrastructure.in.controller;
 
-import com.unisabana.airport.comercio.application.port.in.IAgregarTienda;
 import com.unisabana.airport.comercio.application.port.in.ICobrarArriendoMensual;
 import com.unisabana.airport.comercio.application.port.in.IReubicarTienda;
-import com.unisabana.airport.comercio.application.usecase.TiendaDTO;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,13 +9,11 @@ public class AirportTiendaController {
 
     private final ICobrarArriendoMensual cobrarArriendoMensual;
     private final IReubicarTienda reubicarTienda;
-    private final IAgregarTienda agregarTienda;
 
 
-    public AirportTiendaController(ICobrarArriendoMensual cobrarArriendoMensual, IReubicarTienda reubicarTienda, IAgregarTienda agregarTienda) {
+    public AirportTiendaController(ICobrarArriendoMensual cobrarArriendoMensual, IReubicarTienda reubicarTienda) {
         this.cobrarArriendoMensual = cobrarArriendoMensual;
         this.reubicarTienda = reubicarTienda;
-        this.agregarTienda = agregarTienda;
     }
 
     @PutMapping("/aeropuertoSeguimiento/reubicarTienda")
@@ -28,11 +24,6 @@ public class AirportTiendaController {
     @PostMapping("/aeropuertoSeguimiento/cobrarArriendo")
     public void cobrarArriendoMensual(@RequestParam String nombreTienda) {
         cobrarArriendoMensual.cobrarArriendoMensual(nombreTienda);
-    }
-
-    @PostMapping("/aeropuertoSeguimiento/agregarTienda")
-    public void cobrarArriendoMensual(@RequestBody TiendaDTO dto) {
-        agregarTienda.agregarTienda(dto);
     }
 
 }
